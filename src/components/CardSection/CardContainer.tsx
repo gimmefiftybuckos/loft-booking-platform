687b2a539d52eed9832982ad35ef14de45cff901
Dmitriy Birichev <dbirichev@gmail.com>
2024-08-08 17:04:31 +0300

    feat: inital basic card component layout

--- src/components/CardSection/CardContainer.tsx
+++ src/components/CardSection/CardContainer.tsx
@@ -72,6 +72,23 @@ export const CardSection = () => {
                         / час
                      </Text>
                   </div>
+                  <div className={clsx(styles.room)}>
+                     <div className={clsx(styles.room__people)}>
+                        <Text size='14' as={'p'}>
+                           25 чел
+                        </Text>
+                     </div>
+                     <div className={clsx(styles.room__seats)}>
+                        <Text size='14' as={'p'}>
+                           38 мест
+                        </Text>
+                     </div>
+                     <div className={clsx(styles.room__area)}>
+                        <Text size='14' as={'p'}>
+                           210 m²
+                        </Text>
+                     </div>
+                  </div>
                </div>
             </article>
          </div>
