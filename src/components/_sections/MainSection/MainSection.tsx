314a2959aba8b7f0be7308270d805a09df474c74
Dmitriy Birichev <dbirichev@gmail.com>
2024-08-26 21:58:42 +0300

    feat: add Arrow component

--- src/components/_sections/MainSection/MainSection.tsx
+++ src/components/_sections/MainSection/MainSection.tsx
@@ -17,6 +17,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../../store';
 import { selectionParamsType } from '../../../types';
 import { setType, setToSearchType } from '../../../store/cardCatalogSlice';
+import { Arrow } from '../../_reusable/Arrow';
 
 type ModalContextType = ((key: number) => void) | null;
 
@@ -78,6 +79,7 @@ export const MainSection = () => {
             <h1 className={clsx(styles.title)}>
                Более 500 площадок для вашего мероприятия
             </h1>
+
             <div
                className={clsx(
                   styles.container,
@@ -92,16 +94,8 @@ export const MainSection = () => {
                         className={clsx(styles.button)}
                      >
                         {selectionReducer(item) || item}
-                        <img
-                           className={clsx(
-                              styles.arrow,
-                              openModalKey === index ? styles.arrow_open : null
-                           )}
-                           src='/assets/down.svg'
-                           alt='Dropdown Icon'
-                           width='16'
-                           height='16'
-                        />
+                        <Arrow num={openModalKey} index={index} />
+
                         <Modal isOpen={openModalKey === index}>
                            <ModalContext.Provider value={toggleModal}>
                               <ModalContent name={item} />
